2b2c0b66ad535161c4dae1c400ee35c759af47ce
Mubarek Mohammed <38358880+mube1@users.noreply.github.com>
2024-01-08 00:24:31 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,23 @@ mod sumcheck;
 
 
 fn main() {
+    // x1*x1*x1*2+x1*x3+x2*x3
     let args: Vec<String> = env::args().collect();
     let expression :&String = &args[1];
+    // let first_value :i32 =;
     // let polynomial=parser::Polynomial::new(expression);    
-    // let mut proover = 
-    let  verifier=sumcheck::Verifier{p:sumcheck::Proover::new(expression.to_string()),r:[2,3,6].to_vec()};
-    // struct Verifier { p:Proover ,  r: Vec<i32>}
- 
-
-    if sumcheck::sumcheck_protocol(verifier,verifier.p.polynomial.nov.into()){
+    let mut proover = sumcheck::Proover::new(expression.to_string());
+    proover.polynomial.count_variables();
+    proover.polynomial.find_terms();
+    proover.first_value= args[2].parse::<i32>().unwrap();
+    // println!("first value read {}", proover.first_value*1);
+    let prime:i32=proover.polynomial.generate_prime();
+    
+    let mut verifier=sumcheck::Verifier{p:proover,
+        rv:vec![],
+        prime:prime}; 
+    println!("generated prime {}", verifier.prime);
+    if sumcheck::sumcheck_protocol(&mut verifier){
         println!("Accept");
     }
     else{
